c02029342b0102f88ac930c426fbdcd653115e70
lining <lo@qq.com>
2024-11-22 20:02:07 +0800

    chore: 配置 mock 数据

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,15 @@ import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 import 'virtual:svg-icons-register'
 import gloablComponent from '@/components/index'
 import '@/styles/index.scss'
+import axios from 'axios'
+axios({
+  url: '/api/user/login',
+  method: 'post',
+  data: {
+    username: 'admin',
+    password: '111111',
+  },
+})
 
 const app = createApp(App)
 
