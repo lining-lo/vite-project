f9b70f958884a67b6fc3490f33f227f1061ce7e7
lining <lo@qq.com>
2024-12-09 10:23:43 +0800

    chore: 品牌管理模块数据展示

--- src/api/user/type.ts
+++ src/api/user/type.ts
@@ -1,37 +1,27 @@
-//定义用户相关数据的ts类型
-//用户登录接口携带参数的ts类型
+//登录接口需要携带参数类型
 export interface loginFormData {
   username: string
   password: string
 }
 
-//定义全部接口返回数据都拥有ts类型
+//定义全部接口返回数据都有的数据类型
 export interface ResponseData {
   code: number
   message: string
   ok: boolean
 }
-
-interface dataType {
-  token?: string
-  message?: string
-}
 //定义登录接口返回数据类型
-export interface loginResponseData {
-  code: number
-  data: dataType
+export interface loginResponseData extends ResponseData {
+  data: string
 }
 
-interface checkUserType {
-  routes: string[]
-  buttons: string[]
-  roles: string[]
-  username: string
-  avatar: string
-}
-//定义获取用户信息返回数据类型
-export interface userInfoReponseData extends ResponseData {
+//定义获取用户信息返回的数据类型
+export interface userInfoResponseData extends ResponseData {
   data: {
-    checkUser: checkUserType
+    routes: string[]
+    button: string[]
+    roles: string[]
+    name: string
+    avatar: string
   }
 }
