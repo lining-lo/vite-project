9fa26d28ba5120264b0056e0f0757746d1f7ccba
lining <lo@qq.com>
2024-12-10 19:25:24 +0800

    chore: 属性管理路由静态组件

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,9 @@
 import SvgIcon from '@/components/SvgIcon/index.vue'
 import type { App, Component } from 'vue'
+import Category from './Category/index.vue'
 //引入element-plus提供全部图标组件
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
-const components: { [name: string]: Component } = { SvgIcon }
+const components: { [name: string]: Component } = { SvgIcon, Category }
 export default {
   install(app: App) {
     Object.keys(components).forEach((key: string) => {
